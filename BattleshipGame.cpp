5acc256d721a9eedabd5ddf18d1e5383c88ad59c
vkuryakov <56251513+vkuryakov@users.noreply.github.com>
2021-04-10 13:19:08 +0300

    add menu and function for autoset ships;

--- BattleshipGame.cpp
+++ BattleshipGame.cpp
@@ -12,18 +12,31 @@ const char START_SYMBOL = 'А';
 const string EMPTY_HOR_CELL = "     ";
 const string TWO_SPACES = "  ";
 
-struct APlayer {
-    string name;
+// Режим компьютера: 
+// 0 - тупой (стреляет по случайным клеткам);
+// 1 - поумней (стреляет по алгоритму);
+int computer_mode = 0;
+
+void show_menu();
+
+struct Player {
     // Координаты выстрелов:
     // true - был выстрел
     // false - не было выстрела
-    bool shoots[10][10];
+    bool enemy_field[FIELD_SIZE][FIELD_SIZE];
 
     // Координаты кораблей
     // true - есть корабль на клетке
     // false - нет корабля на клетке
-    bool shipCells[10][10];
+    bool own_field[FIELD_SIZE][FIELD_SIZE];
 };
+
+struct Point {
+    int x, y;
+};
+
+Player player1 = {};
+const Player player2 = {};
 
 void draw_field_header() {
     cout << EMPTY_HOR_CELL;
@@ -92,7 +105,7 @@ void draw_row(int num) {
     cout << endl;
 }
 
-void show_field(APlayer player) {
+void show_field() {
     system("cls");
     // Выводим буквы столбцов первого поля
     draw_field_header();
@@ -117,28 +130,189 @@ void show_field(APlayer player) {
     }
 }
 
+void show_computer_difficulty_dialog() {
+    int input_var;
+    do {
+        system("cls");
+        cout << "\t\tМорской бой." << endl;
+        cout << "1. Легкий" << (computer_mode == 0 ? ".\t+" : ".") << endl;
+        cout << "2. Сложный" << (computer_mode == 1 ? ".\t+" : ".") << endl;
+        cout << "0. Назад." << endl;
+        cin >> input_var;
+        switch (input_var) {
+            case 1:
+                computer_mode = 0;
+                break;
+            case 2:
+                computer_mode = 1;
+                break;
+            default:
+                break;
+        }
+    } while (input_var != 0);
+    show_menu();
+}
+
 void show_menu() {
     int input_var;
     do {
         system("cls");
-        cout << "\t\tBattleship The Game." << endl;
-        cout << "1. Player vs Computer." << endl;
-        cout << "2. Computer vs Computer." << endl;
-        cout << "0. Exit." << endl;
+        cout << "\tМорской бой." << endl;
+        cout << "1. Человек против компьютера." << endl;
+        cout << "2. Компьютер против компьютера." << endl;
+        cout << "3. Установить сложность компьютера." << endl;
+        cout << "0. Выход." << endl;
         cin >> input_var;
-    } while (input_var < 0 || input_var > 2);
+    } while (input_var < 0 || input_var > 3);
+    switch (input_var) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            show_computer_difficulty_dialog();
+            break;
+        default:
+            break;
+    }
+}
+
+bool is_coords_valid(bool field[FIELD_SIZE][FIELD_SIZE], int x, int y, int direction, int deck) {
+    if (field[x][y]) return false;
+    int x_cur, y_cur;
+    for (int i = -1; i < 2; i++) {
+        x_cur = x + i;
+        if (x_cur < 0 || x_cur > FIELD_SIZE) continue;
+        for (int j = -1; j < 2; j++) {
+            if (i == 0 && j == 0) continue;
+            if (deck > 0 
+                && ((direction == 0 && i == -1 && j == 0)
+                    || (direction == 1 && i == 0 && j == 1)
+                    || (direction == 2 && i == 1 && j == 0)
+                    || (direction == 3 && i == 0 && j == -1))) {
+                continue;
+            }
+            
+            y_cur = y + i;
+            if (field[x_cur][y_cur]) return false;
+        }
+    }
+    return true;
+}
+
+bool is_ship_coords_valid(bool field[FIELD_SIZE][FIELD_SIZE], Point p1, Point p2) {
+    if (p1.x < -1 || p1.x > FIELD_SIZE
+        || p1.y < -1 || p1.y > FIELD_SIZE
+        || p2.x < -1 || p2.x > FIELD_SIZE
+        || p2.y < -1 || p2.y > FIELD_SIZE) return false;
+    
+    for (int i = p1.x; i <= p2.x && i > 0 && i < FIELD_SIZE - 1; i++) {
+        for (int j = p1.y; j <= p2.y && j > 0 && j < FIELD_SIZE - 1; j++) {
+            if (field[i][j]) return false;
+        }
+    }
+    return true;
+}
+
+void clear_player(Player p) {
+    for (int i = 0; i < FIELD_SIZE; i++) {
+        for (int j = 0; j < FIELD_SIZE; j++) {
+            p.own_field[i][j] = false;
+            p.enemy_field[i][j] = false;
+        }
+    }
+}
+
+void set_ships_auto(Player &p) {
+    // служит для вычисления кол-ва кораблей (например, 1 единица 4-палубных кораблей = 4 + 1 = 5, 2 единицы 3-палубных = 2 + 3 = 5 и т.д.)
+    const int cells_count_sum = 5;
+    int ships_count, x, y, dir;
+    Point p1, p2;
+    // кол-во палуб корабля
+    for (int i = 1; i < cells_count_sum; i++) {
+        ships_count = cells_count_sum - i;
+        // корабли
+        for (int j = 0; j < ships_count; j++) {
+
+            do {
+                x = rand() % FIELD_SIZE;
+                y = rand() % FIELD_SIZE;
+                // Направление:
+                // 0 - горизонтальное (слева направо);
+                // 1 - вертикальное (сверху вниз);
+                dir = rand() % 2;
+
+                p1.x = x - 1;
+                p1.y = y - 1;
+                
+                if (dir == 0) {
+                    p2.x = x + i;
+                    p2.y = y + 1;
+                }
+                else {
+                    p2.x = x + 1;
+                    p2.y = y + i;
+                }
+            } while (!is_ship_coords_valid(p.own_field, p1, p2));
+            for (int k = 0; k < i; k++) {
+                if (dir == 0) {
+                    p.own_field[x + k][y] = true;
+                }
+                else {
+                    p.own_field[x][y + k] = true;
+                }
+            }
+            
+            // рассчет координат палуб
+            //for (int k = 0; k < i; k++) {
+            //    do {
+            //        if (k == 0) {
+            //            x = rand() % FIELD_SIZE;
+            //            y = rand() % FIELD_SIZE;
+            //            if (i > 1) {
+            //                if (x == 0) {
+            //                    if (y == 0) dir = rand() % 2 + 1;
+            //                    else if (y == FIELD_SIZE - 1) dir = rand() % 2;
+            //                    else dir = rand() % 3
+            //                }
+            //                else if (x == FIELD_SIZE - 1) {
+            //                    if (y == 0) dir = rand() % 2 + 1;
+            //                    if (y == FIELD_SIZE - 1) dir = rand() % 2;
+            //                }
+            //            }
+            //            else {
+            //                dir = 0;
+            //            }
+            //            
+            //            dir = i > 1 ? rand() % 4 : 0;
+            //        }
+            //        else {
+            //            
+            //        }
+            //        
+            //    } while (!is_coords_valid(p.own_field, x, y, dir, k));
+            //    p.own_field[x][y] = true;
+            //}
+        }
+    }
+}
+
+void run_human_vs_comp() {
 
 }
+
+void run_comp_vs_comp() {
+
+}
+
 
 int main()
 {
     setlocale(LC_CTYPE, "Russian");
     srand(time(NULL));
-    //HWND console = GetConsoleWindow();
-    //RECT r;
-    //GetWindowRect(console, &r);
-    //MoveWindow(console, r.left, r.top, 1500, 600, TRUE);
-    show_field({});
+    set_ships_auto(player1);
+    show_menu();
+    //show_field();
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
